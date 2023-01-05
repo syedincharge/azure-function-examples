0bafff156d5ef6c12c7ea00e803e336b09fc8406
syedi786 <syedrizvidev12@gmail.com>
2023-01-05 17:37:16 -0600

    new function commits

---
+++ src/main/java/org/example/func/Function_azure_webhook.java
@@ -0,0 +1,32 @@
+package org.example.func;
+
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function_azure_webhook {
+    /**
+     * This function listens at endpoint "/api/Function-azure-webhook". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/Function-azure-webhook
+     * 2. curl {your host}/api/Function-azure-webhook?name=HTTP%20Query
+     */
+    @FunctionName("Function-azure-webhook")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request and deployed.");
+
+        // Parse query parameter
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
+    }
+}
